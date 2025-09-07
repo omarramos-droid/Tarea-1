9bd773bcd8c47a3a009c7a137ecec7e18a59756f
mismorgano <mismorgano@gmail.com>
2025-09-06 23:00:05 -0600

    add: src package
    The idea is that is this folder we put our code to reuse it.
    - __init__.py to make a package.
    - config.py to declare global variables.

---
+++ src/config.py
@@ -0,0 +1,4 @@
+from pathlib import Path
+
+data_url = "https://datapub.gfz-potsdam.de/download/10.5880.GFZ.4.3.2023.002sdfsd/2023-001_ISONET-Project-Members_13C_Data.zip"
+data_dir = Path("./data")
